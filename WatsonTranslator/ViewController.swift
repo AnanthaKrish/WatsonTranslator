afe1f1a0ec4c387f15059c175c61344186f6c3f1
Anthony Oliveri <oliveri@us.ibm.com>
2019-01-12 16:01:41 -0600

    Brand new Xcode project
    Single page app that includes unit tests and UI tests

---
+++ WatsonTranslator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatsonTranslator
+//
+//  Created by Anthony Oliveri on 1/12/19.
+//  Copyright © 2019 IBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
