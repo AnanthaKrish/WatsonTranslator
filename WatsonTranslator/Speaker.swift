bfe98be04542e40d0f5ed38b16315a2c2455872e
Anthony Oliveri <oliveri@us.ibm.com>
2019-01-27 19:59:31 -0600

    Use the correct voice for the output language
    Only some languages can be converted to speech

--- WatsonTranslator/Speaker.swift
+++ WatsonTranslator/Speaker.swift
@@ -14,13 +14,22 @@ class Speaker {
 
     let textToSpeech: TextToSpeech
     var audioPlayer: AVAudioPlayer?
+    var supportedVoices: [Voice]?
 
     init() {
         textToSpeech = TextToSpeech(apiKey: Credentials.TextToSpeechAPIKey)
+        loadAllVoices()
     }
 
-    func readAloud(text: String) {
-        textToSpeech.synthesize(text: text, accept: "audio/wav", voice: nil) {
+    func readAloud(text: String, language: Language) {
+        guard let supportedVoices = supportedVoices else { return }
+
+        let voice = supportedVoices.first { voice -> Bool in
+            return voice.languageID == language.rawValue
+        }
+        guard voice != nil else { return }
+        
+        textToSpeech.synthesize(text: text, accept: "audio/wav", voice: voice?.name) {
             response, error in
 
             if let error = error {
@@ -41,5 +50,34 @@ class Speaker {
                 print(error)
             }
         }
+    }
+
+    func loadAllVoices() {
+        // Don't need to make an unnecessary network call if allVoices is already loaded
+        guard supportedVoices == nil else { return }
+
+        textToSpeech.listVoices() {
+            response, error in
+
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+
+            guard let voices = response?.result?.voices else {
+                print("Failed to retrieve voices")
+                return
+            }
+
+            self.supportedVoices = voices
+        }
+    }
+}
+
+
+extension Voice {
+    // Used to match a Voice object to the Language type
+    var languageID: String {
+        return String(self.language.prefix(2))
     }
 }
